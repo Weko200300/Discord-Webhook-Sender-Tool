73a879bf2a9f29569e2bf3203fefea85ffafced8
Weko200300 <92626162+Weko200300@users.noreply.github.com>
2023-03-20 04:20:16 +0100

    Discord WebHook Sender
    Discord WebHook Sender Tool Code

---
+++ Discord Web Hook/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Discord_Web_Hook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Location = new System.Drawing.Point(16, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1185, 130);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Senden";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Turquoise;
+            this.textBox1.Location = new System.Drawing.Point(16, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1185, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Gebe einen Text ein denn der WebHook Senden soll";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Turquoise;
+            this.textBox2.Location = new System.Drawing.Point(16, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1185, 26);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "WebHook URL";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Turquoise;
+            this.textBox3.Location = new System.Drawing.Point(16, 44);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(1185, 26);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "WebHook Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(1213, 249);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Discord Web Hook";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Discord Web Hook/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Discord_Web_Hook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static void sendwebhooksender(string Url, string msg, string Username)
+        {
+            Https.Post(Url, new System.Collections.Specialized.NameValueCollection()
+            {
+                {
+                    "Username",
+                    Username
+                },
+                {
+                    "content",
+                    msg
+                }
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sendwebhooksender(textBox2.Text, textBox1.Text, textBox3.Text);
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {}
+    }
+}
